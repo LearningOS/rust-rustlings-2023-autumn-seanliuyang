79a4da4fd67ddc3ac5f9892ff73f8e86969ffe22
seanliuyang <seanliuyang@126.com>
2023-10-20 05:47:20 +0800

    '2023-10-20'

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -18,11 +18,13 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use crate::List::Cons;
+use crate::List::Nil;
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    //    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +37,14 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    //todo!()
+    // Cons(0, Box::new(Nil))
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    //todo!()
+    List::Cons(0, Box::new(Nil))
 }
 
 #[cfg(test)]
